79b0375104db62595e16ece3306aef33d0275c35
Julian <julian_delisio@hotmail.com>
2023-08-31 23:28:42 -0300

    se termina header y footer

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,12 +17,12 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          - Find, book, rent a car—quick and super easy! -
+           Find, book, rent a car—quick and super easy! 
         </h1>
 
         <p className="hero__subtitle">
-          - Streamline your car rental experience with our effortless booking 
-          process -
+           Streamline your car rental experience with our effortless booking 
+          process 
         </p>
 
         <CustomButton
